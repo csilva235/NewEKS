559e24b4e38539cc0e3e5eb1a70e120f0d1d333f
Chris Silva <csilva235@gmail.com>
2022-09-09 09:40:56 -0500

    Add files via upload

---
+++ front-end/src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  resourceApi: "https://api.dev.cmcloudlab686.info/resources-api",
+  inventoryApi: "https://api.dev.cmcloudlab686.info/inventory-api",
+  clientApi: "https://api.dev.cmcloudlab686.info/clients-api",
+  rentingApi: "https://api.dev.cmcloudlab686.info/renting-api" 
+};
---
+++ front-end/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  resourceApi: "https://api.cmcloudlab686.info/resources-api",
+  inventoryApi: "https://api.cmcloudlab686.info/inventory-api",
+  clientApi: "https://api.cmcloudlab686.info/clients-api",
+  rentingApi: "https://api.cmcloudlab686.info/renting-api" 
+};
